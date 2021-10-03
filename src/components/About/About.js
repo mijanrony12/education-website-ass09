1a4fec71c8e1299e82963010e293ea942c746d8c
mijanrony12e <mijaninfo12@gmail.comm>
2021-10-04 03:06:10 +0600

    about and contact

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -1,9 +1,13 @@
 import React from 'react';
-
+import './About.css'
 const About = () => {
     return (
-        <div>
-            about
+        <div className="d-flex justify-content-center text-center my-5">
+              <div className="about-section">
+                      <h1>do you know <span>About Us</span></h1>
+                     <h4>please read this content</h4>
+                     <p>In Mesopotamia, the early logographic system of cuneiform script took many years to master. Thus only a limited number of individuals were hired as scribes to be trained in its reading and writing. Only royal offspring and sons of the rich and professionals such as scribes, physicians, and temple administrators, were schooled.[5] Most boys were taught their father's trade or were apprenticed to learn a trade.[6][page needed] Girls stayed at home with their mothers to learn housekeeping and cooking, and to look after the younger children. Later, when a syllabic script became more widespread, more of the Mesopotamian population became literate. Later still in Babylonian times there were libraries in most towns and temples; an old Sumerian proverb averred "he who would excel in the school of the scribes must rise with the dawn." There arose a whole social class of scribes, mostly employed in agriculture, but some as personal secretaries or lawyers.[7] Women as well as men learned to read and write, and for the Semitic Babylonians, this involved knowledge of the extinct Sumerian language, and a complicated and extensive syllabary. Vocabularies, grammars, and interlinear translations were compiled for the use of students, as well as commentaries on the older texts and explanations of obscure words and phrases. Massive archives of texts were recovered from the archaeological contexts of Old Babylonian scribal schools known as edubas (2000–1600 BCE), through which literacy was disseminated. The Epic of Gilgamesh, an epic poem from Ancient Mesopotamia is among the earliest known works of literary fiction. The earliest Sumerian versions of the epic date from as early as the Third Dynasty of Ur</p>
+              </div>
         </div>
     );
 };
--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,9 +1,21 @@
 import React from 'react';
-
+import './Contact.css'
 const Contact = () => {
     return (
-        <div>
-            contact
+        <div className="d-flex justify-content-center text-center my-5">
+               <div className="contact-section">
+                            <h1>institute Name: Nirjhar Education</h1>
+                            <p>we are try our best services.. we are very friendly.our sucess rate 80%</p>
+                            <h1>Contact</h1>
+                            <h5>OffLine:- 01425-748596</h5>
+                            <h5>OnLine:- Nirjhar17@gmail.com</h5>
+                            <h1>Address</h1>
+                            <h5>Mohammadpur , block/A</h5>
+                            <h5>Road numder:- 13/B</h5>
+                            <h5>Dhaka-1212</h5>
+                            <h4>thanks for contact with us</h4>
+                
+                </div>
         </div>
     );
 };
