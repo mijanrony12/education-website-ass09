3ddf35f8c677ff7110c4412f17dd7f8755cd0b2a
mijanrony12e <mijaninfo12@gmail.comm>
2021-10-04 03:23:36 +0600

    notfound section

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
+import './NotFound.css'
 const NotFound = () => {
     return (
-        <div>
-              404
+        <div className="notFound-section text-center">
+             <div className="content">
+                    <h1>Your page not found-----'404'</h1>
+                   <p> please check your url</p>
+                <Link to="/home">
+                     <button>Back Home Page</button>
+                </Link>
+              </div>
         </div>
     );
 };
