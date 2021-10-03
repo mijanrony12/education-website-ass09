9f5c95ad66d9b6006ace4af7503c14b9600d9e3d
mijanrony12e <mijaninfo12@gmail.comm>
2021-10-04 02:16:38 +0600

    home done

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,18 +1,40 @@
 import React from 'react';
+import { useEffect } from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
+import Service from '../Service/Service';
 import './Home.css'
 const Home = () => {
-
+       const [ services, setServices ] = useState([])
+    
+    useEffect(() => {
+        fetch('./FakeData.json')
+            .then(res => res.json())
+        .then(data => setServices(data))
+   },[])
     return (
-        <div className="banner">
-            <div class="banner-text">
-                <h1 >I am Sirajum Munir Nirjhar</h1>
-                <p>i was IELTS master Trainer  and have 14 years Experiene</p>
-                <Link to="/about">
-                       <button>About Me</button>
-                </Link>
+        <div>
+             <div className="banner">
+                <div class="banner-text">
+                    <h1 >I am Sirajum Munir Nirjhar</h1>
+                    <p>i was IELTS master Trainer  and have 14 years Experiene</p>
+                    <Link to="/about">
+                        <button>About Me</button>
+                    </Link>
+                </div>
             </div>
-      </div>
+            <div>
+            <h1 className="text-center mt-4  service"> Our Service</h1>
+            <div className="row gy-4 m-5">
+                  {
+                    services.slice(0,3).map(service => <Service
+                        key={ service.id }
+                        service={service}
+                    ></Service>)
+                   }
+            </div>
+        </div>
+       </div>
     );
 };
 
