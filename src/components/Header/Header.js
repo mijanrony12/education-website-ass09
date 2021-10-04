9b135ba221dd538200d7377703e955e1d26c437e
mijanrony12e <mijaninfo12@gmail.comm>
2021-10-04 06:18:35 +0600

    recheck

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,13 +12,15 @@ const Header = () => {
             <div className="container menu">
                     <div>
                         <h2>Nirjhar <span>Education</span></h2>
-                </div>
-                    <nav>
+                 </div>
+                  
+                        <nav>
                             <NavLink activeStyle={hoverStyle} to="/home">Home</NavLink>
                             <NavLink activeStyle={hoverStyle} to="/about">About</NavLink>
                             <NavLink activeStyle={hoverStyle} to="/services">Services</NavLink>
                             <NavLink activeStyle={hoverStyle} to="/contact">Contact</NavLink>
                     </nav>
+                   
                </div>
         </div>
     );
